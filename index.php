1315fb5a930424427d687738d9848477825109c6
INGASOFT SISTEMAS INGASOFT <rodrigo@MacBook-Air-de-INGASOFT.local>
2024-06-07 17:28:00 -0300

    älteracoes

--- index.php
+++ index.php
@@ -6,6 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Video Downloader</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        #div_resultado_sucesso {
+            display: none;
+            margin-top: 20px;
+        }
+
+        #video_metadata {
+            margin-top: 20px;
+        }
+
+        .video-info {
+            margin-top: 10px;
+        }
+    </style>
 </head>
 
 <body>
@@ -21,35 +35,31 @@
                 </div>
             </div>
         </form>
-
         <hr>
-
-        <div id="div_resultado_sucesso" class="d-flex justify-content-center">
-            <img id="video_thumbnail" src="" alt="Video Thumbnail" class="img-fluid mb-2" class="img-responsive" style="max-height: 50px;">
-            <div>
-                <a id="download_sd" class="btn btn-primary mx-2" href="#" download="video_sd.mp4">SD</a>
-                <a id="download_hd" class="btn btn-primary mx-2" href="#" download="video_hd.mp4">HD</a>
+        <div id="div_resultado_sucesso">
+            <center>
+                <video id="video_preview" controls style="max-height: 100%; width: 300px;" class="img-fluid mb-2" poster=""></video>
+            </center>
+            <div class="d-flex justify-content-center">
+                <a id="download_sd" class="btn btn-success mx-2" href="#" download="video_sd.mp4">Download SD</a>
+                <a id="download_hd" class="btn btn-info mx-2" href="#" download="video_hd.mp4">Download HD</a>
+            </div>
+            <div id="video_metadata" class="text-center">
+                <!-- Placeholder for video metadata -->
             </div>
         </div>
     </div>
-
-
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
     <script>
         $(document).ready(function() {
-
-            $("#div_resultado_sucesso").hide();
-
             $("#form_download").submit(function(e) {
                 e.preventDefault();
                 const url_download = $("#url_download").val();
-
                 if (!url_download) {
                     $("#url_download").focus();
                     return;
                 }
-
                 $.ajax({
                     url: "src/video_download.php",
                     method: 'POST',
@@ -58,18 +68,19 @@
                         videoURL: url_download
                     },
                     success: function(data) {
-                        console.log('Success:', data);
                         if (!data.success) {
                             alert(data.msg || "Ocorreu algum erro.");
                             return;
                         }
-
-                        // Update download links and thumbnail
                         $('#download_sd').attr('href', data.links.sd);
                         $('#download_hd').attr('href', data.links.hd);
-                        $('#video_thumbnail').attr('src', data.thumbnail).show();
+                        //$('#video_thumbnail').attr('src', data.thumbnail).show();
 
-                        // Display the success div if hidden
+                        $('#video_preview').attr('poster', data.thumbnail); // Set the poster attribute
+                        $('#video_preview').attr('src', (data.links.hd || data.links.sd));
+
+                        fetchVideoDuration('sd', data.links.sd);
+                        fetchVideoDuration('hd', data.links.hd);
                         $('#div_resultado_sucesso').show();
                     },
                     error: function(error) {
@@ -78,6 +89,25 @@
                 });
             });
         });
+
+        function fetchVideoDuration(quality, videoURL) {
+            var video = document.createElement('video');
+            video.src = videoURL;
+            video.addEventListener('loadedmetadata', function() {
+                const videoInfoHtml = `
+                    <div class="video-info">
+                        <strong>${quality.toUpperCase()} Info:</strong><br>
+                        Height: ${video.videoHeight}px<br>
+                        Width: ${video.videoWidth}px<br>
+                        Duration: ${video.duration.toFixed(2)} seconds
+                    </div>`;
+                $('#video_metadata').append(videoInfoHtml);
+            });
+            video.addEventListener('error', function() {
+                console.error('Error loading video');
+                alert('Failed to load video metadata.');
+            });
+        }
     </script>
 </body>
 
